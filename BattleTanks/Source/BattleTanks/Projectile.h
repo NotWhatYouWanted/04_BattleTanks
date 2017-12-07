ed4ca9ea225ee85fad5703398be6db6dc38ede1c
LAUSTEN-GAMERIG\User <laurie.f.austen@btinternet.com>
2017-12-07 15:02:30 +0000

    Impact blast effect added, also tidied .git

--- BattleTanks/Source/BattleTanks/Projectile.h
+++ BattleTanks/Source/BattleTanks/Projectile.h
@@ -1,11 +1,15 @@
 // Fill out your copyright notice in the Description page of Project Settings.
+
 #pragma once
 #include "GameFramework/ProjectileMovementComponent.h"
 #include "Runtime/Engine/Classes/Particles/ParticleSystemComponent.h"
 #include "Runtime/Engine/Classes/Components/StaticMeshComponent.h"
+#include "Runtime/Engine/Classes/Components/PrimitiveComponent.h"
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "Projectile.generated.h"
+
+
 UCLASS()
 class BATTLETANKS_API AProjectile : public AActor
 {
@@ -15,21 +19,26 @@ public:
 	// Sets default values for this actor's properties
 	AProjectile();
 
+	void LaunchProjectile(float Speed);
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 	UProjectileMovementComponent* MovementComponent = nullptr;
 
-	UPROPERTY(VisibleAnywhere)
+	UPROPERTY(VisibleAnywhere, Category = Components)
 		UStaticMeshComponent* CollisionMesh = nullptr;
 
-	UPROPERTY(EditAnywhere)
+	UPROPERTY(EditAnywhere, Category = Components)
 		UParticleSystemComponent* LaunchBlast = nullptr;
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
 
-	void LaunchProjectile(float Speed);
+	UPROPERTY(EditAnywhere, Category = Components)
+		UParticleSystemComponent* ImpactBlast = nullptr;
+
+private:	
+
+	UFUNCTION()
+		void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
 	
 };
